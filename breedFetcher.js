07666ca85ea9649d6e92d4b33e75000905ba1f0b
siopao2001 <sio_pao2001@yahoo.ca>
2021-08-19 20:24:42 +0000

    2nd commit

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,14 +2,15 @@ const request = require('request');
 let keyWord = process.argv.slice(2);
 request(`https://api.thecatapi.com/v1/breeds/search?q=${keyWord}`, (error, response, body) => {
   if (error) {
-    console.log('Error:', error);
-  };
-  const data = JSON.parse(body);
+    console.log("error:", error);
+  } else {
+   const data = JSON.parse(body);
   if (data.length === 0) {
     console.log("Breed not found");
   } else {
     console.log(data[0].description);
   }
+}
 });
  
 
